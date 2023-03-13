7d8942db7feafb0a882f8f7962b271ac23517e2b
Hikmat Bangash <hikmatkhanbangash@gmail.com>
2023-03-14 00:52:42 +0500

    resolve error

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import HomeScreen from "@/components/HomeScreen/HomeScreen";
 import Header from "@/components/Header";
 import Timeline from "@/app/complaint/timeline/[status]/page";
 import { Inter } from "next/font/google";
-import Authentication from "@/components/auth/Authentication";
+// import Authentication from "@/components/auth/Authentication";
 import Complaints from "@/components/Complaints";
 
 export default function Home() {
