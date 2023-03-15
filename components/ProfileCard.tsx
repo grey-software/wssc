4f25ea51c88b69c6cdd0181203e743dbe011b65f
AyeshaAurangzeb <ayeshaaurangzeb7@gmail.com>
2023-03-15 21:52:39 +0500

    ProfileCard created

---
+++ components/ProfileCard.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const ProfileCard = () => {
+  return (
+    <div className='text-center mt-20'>
+        <h1>Profile Card</h1>
+
+    </div>
+  )
+}
+
+export default ProfileCard
