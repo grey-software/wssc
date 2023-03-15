6a973d4e88f4006f9cf654a9fc84d90c3baf8924
Hikmat Bangash <hikmatkhanbangash@gmail.com>
2023-03-15 10:49:48 +0500

    modified complaint form and notification icon

--- components/Header.tsx
+++ components/Header.tsx
@@ -31,9 +31,9 @@ const Header = () => {
           />
           {/* notification indicator */}
 
-          <div className="absolute right-0 flex h-3 w-3">
-            <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-blue-600 opacity-95"></span>
-            <span className="relative inline-flex rounded-full h-3 w-3 bg-blue-600"></span>
+          <div className="absolute right-1 flex h-2 w-2">
+            <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-500 opacity-95"></span>
+            <span className="relative inline-flex rounded-full h-2 w-2 bg-red-500"></span>
           </div>
         </div>
 
@@ -43,11 +43,7 @@ const Header = () => {
           alt="profileIcon"
           onClick={() => setMenuActive(!menuActive)}
         />
-        
-        {/* <FaUserCircle
-          className="text-[28px] text-primaryColor-500"
-          onClick={() => setMenuActive(!menuActive)}
-        /> */}
+      
       </div>
       {/* Notification window */}
       <div
