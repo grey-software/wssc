7d874fd1bd6a4f358f5f40859acb906af0ebee36
AyeshaAurangzeb <ayeshaaurangzeb7@gmail.com>
2023-03-23 20:57:37 +0500

    Profile card updated without icons

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
   return (
     <>
       <div className="container">
-        {phone ? <HomeScreen /> : <Authentication />}
+        {phone ? <ProfileCard /> : <Authentication />}
       </div>
       {/* <Authentication /> */}
       {/* <Signup /> */}
