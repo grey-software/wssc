168f7769a7d39d20ff65667d1603d2eec34dcd0f
ihtisham914 <shaami.khn321@gmail.com>
2023-03-29 19:50:14 +0500

    Navigation to complaint tracking enabled, feedback pop design improved

--- components/FeedbackRating.tsx
+++ components/FeedbackRating.tsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import toast from "react-hot-toast";
-import {IoCloseSharp} from 'react-icons/io5'
+import { IoCloseSharp } from "react-icons/io5";
+import { HiStar, HiOutlineStar } from "react-icons/hi";
 
 interface Props {
-    feedback: boolean,
-    setfeedback: React.Dispatch<React.SetStateAction<boolean>>
+  feedback: boolean;
+  setfeedback: React.Dispatch<React.SetStateAction<boolean>>;
 }
-const FeedbackRating = ({feedback, setfeedback}:Props) => {
+const FeedbackRating = ({ feedback, setfeedback }: Props) => {
   const [rating, setRating] = useState<number>(0);
 
   const rates: number[] = [1, 2, 3, 4, 5];
@@ -17,30 +18,36 @@ const FeedbackRating = ({feedback, setfeedback}:Props) => {
 
   // ToastifyNotifiction method definition
   const ToastifyNotifiction = () => {
-  
     if (rating > 0) {
-      setfeedback(!feedback)
+      setfeedback(!feedback);
       toast.success("Thanks for your Feedback", {
         position: "top-center",
         style: { width: "auto", height: "auto" },
-        duration: 3000
+        duration: 3000,
+      });
+    } else {
+      toast.error("Your rating help us Improve our service", {
+        position: "top-center",
+        style: { width: "auto", height: "auto" },
+        duration: 3000,
       });
     }
-    else {
-       toast.error("Without ratings, Feedback cannot be submitted", {
-         position: "top-center",
-         style: { width: "auto", height: "auto" },
-         duration: 3000,
-       });
-    }
-  }
+  };
 
   return (
     <>
       <div className="fixed top-0 flex justify-center items-center w-full h-[100vh] backdrop-blur-sm transition-all">
-        <div className=" flex flex-col gap-6 shadow-lg shadow-gray-300 bg-gray-50 px-5 mr-6 py-5 border border-gray-200 rounded-md w-[90%] overflow-hidden">
+        <div className=" flex flex-col gap-6 shadow-lg shadow-gray-300 bg-gray-50 px-5 mr-6 py-8 border border-gray-200 rounded-md w-[90%] overflow-hidden">
           {/* close feedback icon */}
-           <h4 onClick={()=>setfeedback(!feedback)} className="relative -mb-8 -mt-3 -mr-3 flex justify-end  text-2xl text-green-600 font-bold"><IoCloseSharp /></h4>
+          <div className="flex items-center justify-between text-lg font-semibold">
+            <span>FeedBack</span>
+            <h4
+              onClick={() => setfeedback(!feedback)}
+              className="flex justify-end  text-2xl text-green-600 font-bold"
+            >
+              <IoCloseSharp />
+            </h4>
+          </div>
 
           <div className="service ">
             <h1>Are you satisfied with our service?</h1>
@@ -49,17 +56,19 @@ const FeedbackRating = ({feedback, setfeedback}:Props) => {
 
           <div>
             <h1 className="font-semibold">How would you rate us</h1>
-            {rates.map((value) => (
-              <span
-                key={value}
-                onClick={() => handleRatingClick(value)}
-                className="cursor-pointer text-4xl"
-              >
-                <span className="text-yellow-500 outline-none ">
-                  {value <= rating ? "★" : "☆"}
+            <div className="flex items-center ">
+              {rates.map((value) => (
+                <span
+                  key={value}
+                  onClick={() => handleRatingClick(value)}
+                  className="cursor-pointer text-4xl"
+                >
+                  <span className="text-yellow-500 outline-none">
+                    {value <= rating ? <HiStar /> : <HiOutlineStar />}
+                  </span>
                 </span>
-              </span>
-            ))}
+              ))}
+            </div>
           </div>
           {/* feedback input field */}
           <div className="feedback">
@@ -73,7 +82,7 @@ const FeedbackRating = ({feedback, setfeedback}:Props) => {
               id="feedbackInput"
               cols={29}
               rows={5}
-              className="mt-1 rounded-lg border border-green-500 px-2 py-1 hover:border-yellow-500 w-full"
+              className="mt-1 rounded-lg border border-gray-500 px-2 py-1 focus:border-primaryColor-500 w-full"
             ></textarea>
           </div>
           {/* feedback submit button */}
