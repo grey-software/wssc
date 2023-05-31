4a46d4a307205d616b009aeb91c9adf6bcd1d62e
ihtisham914 <shaami.khn321@gmail.com>
2023-05-31 08:10:06 +0500

    Updated Dashboard, Created UI for Complaints with pagination

--- apps/dashboard/app/complaint/[id]/page.tsx
+++ apps/dashboard/app/complaint/[id]/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const page = ({ params }: any) => {
   const id = params.id;
-  return <div>{id}</div>;
+  return <div>ComplaintID: {id}</div>;
 };
 
 export default page;
