3fc37c5c5cf02c078a997ea21ab49c899820e209
Hikmat Bangash <hikmatkhanbangash@gmail.com>
2023-06-06 00:45:59 +0500

    refactored the authorization base role middlewares

--- apps/server/Routes/Complaint.route.ts
+++ apps/server/Routes/Complaint.route.ts
@@ -8,19 +8,19 @@ import {
   CitizenFeedback,
   AssignComplaint,
 } from "../controller/Complaint.controller";
-import { verifyAdmin, verifyUser } from "../middleware/verifyToken";
+import { verifyAdmin, verifyToken } from "../middleware/verifyToken";
 
 const ComplaintRouter: Router = Router();
 
 ComplaintRouter.route("/:id")
-  .get(verifyUser, GetAllComplaints)
-  .get(GetComplaint)
-  .patch(CitizenFeedback)
-  .patch(verifyAdmin, UpdateComplaint)
-  .post(verifyUser, CreateComplaint)
+  .get(verifyToken,GetComplaint)
+  .patch(verifyToken, UpdateComplaint)
+  .post(verifyToken, CreateComplaint)
   .delete(verifyAdmin, DeleteAllcomplaints);
 
-// ComplaintRouter.get("/", GetAllComplaints);
+ComplaintRouter.patch("/feedback/:id", verifyToken, CitizenFeedback)
+ComplaintRouter.get("/", verifyToken, GetAllComplaints);
+
 // ASSIGN COMPLAINT ROUTE
 ComplaintRouter.route("/:supervisorId/:complaintId").patch(AssignComplaint);
 
--- apps/server/middleware/verifyToken.ts
+++ apps/server/middleware/verifyToken.ts
@@ -1,3 +1,72 @@
+// import { NextFunction, Request, Response } from "express";
+// import jwt, { VerifyErrors } from "jsonwebtoken";
+// import { createError } from "../HandleError";
+// import dotenv from "dotenv";
+// dotenv.config();
+
+// // eslint-disable-next-line turbo/no-undeclared-env-vars
+// const JWT: any = process.env.JWT_KEY;
+// // ----- verify citizen token -------------
+// export const verifyToken = async (
+//   req: Request,
+//   res: Response,
+//   next: NextFunction
+// ) => {
+//   const token: any = req.cookies.access_token;
+//   if (!token) return next(createError(401, "You are not authenticated!"));
+
+//   return new Promise<void>((resolve, reject) => {
+//     jwt.verify(token, JWT, (err: VerifyErrors | null, payload: any) => {
+//       if (err) return reject(createError(403, "Token is not valid!"));
+
+//       req.user = payload;
+//       resolve();
+//     });
+//   });
+
+// };
+
+// // ----- verifyUser before making any request --------
+// export const verifyUser = async (
+//   req: Request,
+//   res: Response,
+//   next: NextFunction
+// ) => {
+//   try {
+//     await verifyToken(req, res, next);
+//     if (req.user.id == req.params.id || req.user.isAdmin) {
+//       next();
+//     } else {
+//       return res
+//         .status(403)
+//         .json({ status: 403, message: "You are not authorized!" });
+//     }
+//   } catch (error) {
+//     next(error);
+//   }
+// };
+
+// // ------ verify and create middleware for WSSC Admin as well  ------
+// export const verifyAdmin = async (
+//   req: Request,
+//   res: Response,
+//   next: NextFunction
+// ) => {
+//   try {
+//     await verifyToken(req, res, next);
+//     if (req.user.isAdmin) {
+//       next();
+//     } else {
+//       return res
+//         .status(403)
+//         .json({ status: 403, message: "Sorry, You are not authorized" });
+//     }
+//   } catch (error) {
+//     next(error);
+//   }
+// };
+
+
 import { NextFunction, Request, Response } from "express";
 import jwt, { VerifyErrors } from "jsonwebtoken";
 import { createError } from "../HandleError";
@@ -6,95 +75,52 @@ dotenv.config();
 
 // eslint-disable-next-line turbo/no-undeclared-env-vars
 const JWT: any = process.env.JWT_KEY;
+
 // ----- verify citizen token -------------
-export const verifyToken = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+export const verifyToken = (req: Request, res: Response, next: NextFunction) => {
   const token: any = req.cookies.access_token;
+  console.log(token)
   if (!token) return next(createError(401, "You are not authenticated!"));
 
-  return new Promise<void>((resolve, reject) => {
-    jwt.verify(token, JWT, (err: VerifyErrors | null, payload: any) => {
-      if (err) return reject(createError(403, "Token is not valid!"));
-
-      req.user = payload;
-      resolve();
-    });
-  });
-
-  // jwt.verify(token, JWT, (err: jwt.VerifyErrors | null, user: any) => {
-  //     if (err) return next(createError(403, "Token is not valid!"));
-
-  //     req.citizen = user;
-  //     console.log(req.citizen)
-  //     next()
-  // });
+  try {
+    const payload = jwt.verify(token, JWT);
+    req.user = payload;
+    next();
+  } catch (err) {
+    next(createError(403, "Token is not valid!"));
+  }
 };
 
 // ----- verifyUser before making any request --------
-export const verifyUser = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+export const verifyUser = (req: Request, res: Response, next: NextFunction) => {
   try {
-    await verifyToken(req, res, next);
-    if (req.user.id == req.params.id || req.user.isAdmin) {
-      next();
-    } else {
-      return res
-        .status(403)
-        .json({ status: 403, message: "You are not authorized!" });
-    }
+    verifyToken(req, res, () => {
+      if (req.user.id == req.params.id || req.user.isAdmin) {
+        next();
+      } else {
+        return res
+          .status(403)
+          .json({ status: 403, message: "You are not authorized!" });
+      }
+    });
   } catch (error) {
     next(error);
   }
 };
 
 // ------ verify and create middleware for WSSC Admin as well  ------
-export const verifyAdmin = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+export const verifyAdmin = (req: Request, res: Response, next: NextFunction) => {
   try {
-    await verifyToken(req, res, next);
-    if (req.user.isAdmin) {
-      next();
-    } else {
-      return res
-        .status(403)
-        .json({ status: 403, message: "Sorry, You are not authorized" });
-    }
+    verifyToken(req, res, () => {
+      if (req.user.isAdmin) {
+        next();
+      } else {
+        return res
+          .status(403)
+          .json({ status: 403, message: "Sorry, You are not authorized" });
+      }
+    });
   } catch (error) {
     next(error);
   }
 };
-
-// ----- verifyUser before making any request --------
-// export const verifyUser = (req: Request, res: Response, next: NextFunction) => {
-//     console.log('this is verifyUser section')
-//     verifyToken(req, res, next, () => {
-
-//         if (req.user.id == req.params.id || req.citizen.isAdmin) {
-//             next()
-//         } else {
-//              return res.status(403).json({status: 403, message: "You are not authenticated!"})
-//         }
-//     })
-// }
-
-// // ------ verify and create middleware for WSSC Admin as well  ------
-// export const verifyAdmin = (req: Request, res: Response, next: NextFunction) => {
-//     console.log('this is verifyAdmin section')
-//     verifyCitizenToken(req, res, next, () => {
-//         console.log(req.citizen)
-//         if (req.citizen.isAdmin) {
-//             next();
-//         }
-//         else
-//             return res.status(403).json({ status: 403, message: "Sorry, You are not authorized" })
-//     });
-// }
