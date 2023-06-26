f6818f4deef1e4b086395ad317d14c71b73e8ddb
Hikmat Bangash <hikmatkhanbangash@gmail.com>
2023-06-26 17:51:52 +0500

    modified complaint timeline

--- apps/client/app/page.tsx
+++ apps/client/app/page.tsx
@@ -1,10 +1,9 @@
 "use client";
 import HomeScreen from "@/components/HomeScreen/HomeScreen";
-import { Inter } from "next/font/google";
 import Authentication from "@/components/Auth/Authentication";
 import type { RootState } from "../global_state/store";
 import { useSelector } from "react-redux";
-import DesktopMenu from "@/components/DesktopMenu";
+
 
 export default function Home() {
   const { UserInfo }: any = useSelector((state: RootState) => state.users);
